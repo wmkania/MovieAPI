d356163d187ccf4fd205a6336cada6e47a0ce452
wmkania <42653706+wmkania@users.noreply.github.com>
2021-09-14 21:34:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ app.put('/directors/:name/:deathYear', (req, res) => {
   return director.name === req.params.name });
 
   if (director) {
-    director.deathYear[req.params.deathYear] = parseInt(req.params.deathYear);
+    director.deathYear[req.params.director] = parseInt(req.params.deathYear);
     res.status(201).send('Director ' + req.params.name + ' has died in ' + req.params.deathYear);
   } else {
     res.status(404).send('Director with the name ' + req.params.name + ' was not found.');
