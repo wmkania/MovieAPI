c77890e9d407902e9bc9f405244724a897551c82
wmkania <42653706+wmkania@users.noreply.github.com>
2021-10-01 19:58:10 +0100

    Update models.js

--- models.js
+++ models.js
@@ -9,9 +9,10 @@ let movieSchema = mongoose.Schema({
   },
   Director: {
     Name: String,
-    Bio: String
+    Bio: String,
+    Birth: Date,
+    Death: Date
   },
-  Actors: [String],
   ImagePath: String,
   Featured: Boolean
 });
