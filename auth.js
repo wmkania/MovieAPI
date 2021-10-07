bb33e644cec6344daec60fbb4d052c46a88d3735
wmkania <42653706+wmkania@users.noreply.github.com>
2021-10-07 12:15:20 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+const jwtSecret = 'your_jwt_secret';
 
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
