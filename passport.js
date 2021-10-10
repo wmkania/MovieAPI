04343a3934f3449f0999a9225fa277aa5facea4a
wmkania <42653706+wmkania@users.noreply.github.com>
2021-10-10 01:39:34 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -19,9 +19,13 @@ passport.use(new LocalStrategy({
     }
 
     if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
+      console.log('Incorrect username.');
+      return callback(null, false, {message: 'Incorrect username.'});
     }
+    if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, {message: 'Incorrect password.'});
+        }
 
     console.log('finished');
     return callback(null, user);
