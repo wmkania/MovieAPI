3fd85cb1309c3d2d744ec6b34796553a3b5199de
wmkania <42653706+wmkania@users.noreply.github.com>
2021-10-11 18:01:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,8 +49,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+});
 
 // Routes the main Welcome page.
 app.get('/', (req, res) => {
